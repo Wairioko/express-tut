9e15a1138249c9121ef08ef823d4025e44004911
Wairioko <charlesmungai5@gmail.com>
2024-07-02 15:13:27 +0300

    simple illustration of sessions, simple authentication and sessions handling

--- constants.mjs
+++ constants.mjs
@@ -1,10 +1,10 @@
 
 // List of users
 export const users = [
-    { "id": 1, "username": "munga", "email": "munga@gmail.com" },
-    { "id": 2, "username": "munga1", "email": "munga1@gmail.com" },
-    { "id": 3, "username": "munga2", "email": "munga2@gmail.com" },
-    { "id": 4, "username": "munga3", "email": "munga3@gmail.com" }
+    { "id": 1, "username": "munga", "email": "munga@gmail.com", "password": 123 },
+    { "id": 2, "username": "munga1", "email": "munga1@gmail.com", "password": 124 },
+    { "id": 3, "username": "munga2", "email": "munga2@gmail.com", "password": 125 },
+    { "id": 4, "username": "munga3", "email": "munga3@gmail.com", "password": 126 }
 ];
 
 export const products = [
--- sessions/index.mjs
+++ sessions/index.mjs
@@ -1,7 +1,11 @@
-import session from 'express-session';
 import express from 'express';
+import session from 'express-session';
 import cookieParser from 'cookie-parser';
 import routes from "./products.mjs";
+import { users } from '../constants.mjs';
+import { v4 as uuidv4, v4 } from 'uuid';
+import crypto from 'crypto';
+import { validationResult } from 'express-validator';
 
 const app = express();
 
@@ -44,6 +48,46 @@ app.get('/', (req, res) => {
     req.session.visited = true;
     res.send("Hello world");
 });
+
+app.post('/api/auth/', (req, res) => {
+    const { body: { username, password } } = req;
+    const findUser = users.find((user) => user.username === username);
+    if (!findUser || findUser.password !== password) return res.status(401).send("Wrong Password");
+    req.session.user = findUser;
+    return res.status(200).send(findUser);
+});
+
+app.get('/api/auth/status', (req, res) => {
+    return req.session.user ? res.status(200).send(req.session.user) : res.status(401).send("User not found");
+});
+
+app.post('/api/cart', (req, res) => {
+    const { cartItems } = req.body;
+
+    // Check if the user is authenticated
+    if (!req.session.user) {
+        return res.status(401).send("Unauthorized");
+    }
+
+    // Check if cartItems is defined and is an array
+    if (!Array.isArray(cartItems)) {
+        return res.status(400).send("Invalid cart items");
+    }
+
+    // Generate a random cart ID
+    const cartId = crypto.randomBytes(16).toString("hex");
+
+    // Create the user cart object
+    const userCart = {
+        cartid: cartId,
+        'cart-items': cartItems
+    };
+
+    // Respond with the user cart object
+    res.status(200).send(userCart);
+});
+
+
 
 // Start the server
 app.listen(port, () => console.log(`Listening on port ${port}`));
