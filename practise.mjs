21449cea51bd9eadf759c5b79380120801bce0c1
Wairioko <charlesmungai5@gmail.com>
2024-07-07 19:05:30 +0300

    Practice for node and express

--- practise.mjs
+++ practise.mjs
@@ -1,9 +1,3 @@
-import express from "express";
-
-
-const app = express();
-
-const port = process.env.PORT || 3000;
 
 // app.get("/", (req, res) => {
 //     res.sendStatus(200).send({
@@ -114,99 +108,151 @@ const products = [
 //     }
 // }))
 
-import { Router } from "express";
+// import { Router } from "express";
 
-const router = Router();
-
-
-router.post(
-    "/users/", (req, res) => {
-        const { username, email } = req.params;
-        res.status(200).send(
-            {
-                message:"USER CREATED",
-                user: {
-                    "username": username,
-                    "username": email
-                }
-            }
-        )
-
-    }
-)
-
-router.get("/users", (req, res) => {
-    res.status(200).send(users);
-})
+// const router = Router();
 
 
-router.get("/users/:id", (req, res) => {
-    const {id } = req.params;
-    const parsedID = parseInt(id);
-    const findUser = users.find( user => user.id === parsedID)
-    if(!findUser) return res.status(404).send("User not found");
-    res.status(200).send(findUser);
+// router.post(
+//     "/users/", (req, res) => {
+//         const { username, email } = req.params;
+//         res.status(200).send(
+//             {
+//                 message:"USER CREATED",
+//                 user: {
+//                     "username": username,
+//                     "username": email
+//                 }
+//             }
+//         )
 
-})
+//     }
+// )
 
-router.put("/users/:id", (req, res) => {
-    const {id } = req.query;
-    const {email, username} = req.params
-    const parsedID = parseInt(id);
-    const findUser = users.findIndex(user => user.id === parsedID);
-    if(!findUser) return res.status(404).send("User not found");
-    findUser.email = email;
-    findUser.username = username;
-    res.status(200).send(findUser);
-
-})
-
-router.delete(
-    "/users/:id", (req, res) => {
-        const {id} = req.query;
-        const parsedID = parseInt(id);
-        const findUser = users.findIndex(user => user.id === parsedID);
-        if(!findUser) return res.status(404).send("User not found");
-        users.splice(users[parsedID], 1)
-        res.status(200).send(users)
-    }
-)
+// router.get("/users", (req, res) => {
+//     res.status(200).send(users);
+// })
 
 
+// router.get("/users/:id", (req, res) => {
+//     const {id } = req.params;
+//     const parsedID = parseInt(id);
+//     const findUser = users.find( user => user.id === parsedID)
+//     if(!findUser) return res.status(404).send("User not found");
+//     res.status(200).send(findUser);
 
-router.post("/users", (req, res) => {
-    const { email, name } = req.params;
-    if(!email || !email.isString) return res.status(400).send("Missing or wrong format email")
-    if(!name || !name.isString) return res.status(400).send("Missing or wrong format name")
-    res.status(400).send({
-        user:{
-            "email":email,
-            "username": name
-        }
-})      
-})
+// })
+
+// router.put("/users/:id", (req, res) => {
+//     const {id } = req.query;
+//     const {email, username} = req.params
+//     const parsedID = parseInt(id);
+//     const findUser = users.findIndex(user => user.id === parsedID);
+//     if(!findUser) return res.status(404).send("User not found");
+//     findUser.email = email;
+//     findUser.username = username;
+//     res.status(200).send(findUser);
+
+// })
+
+// router.delete(
+//     "/users/:id", (req, res) => {
+//         const {id} = req.query;
+//         const parsedID = parseInt(id);
+//         const findUser = users.findIndex(user => user.id === parsedID);
+//         if(!findUser) return res.status(404).send("User not found");
+//         users.splice(users[parsedID], 1)
+//         res.status(200).send(users)
+//     }
+// )
 
 
 
-router.post("/products/:productId", centralizedErrorHandling ,(req, res) => {
-    const { productId } = req.query;
-    if(!centralizedErrorHandling){
-        if(productId.isString) res.status(400).send("Product cannot be string")
-        const findProduct = products.findIndex((product) => products.productId === product.productId)
-        res.status(200).send(findProduct)
-    }else{
-        res.send(centralizedErrorHandling)
-    }
+// router.post("/users", (req, res) => {
+//     const { email, name } = req.params;
+//     if(!email || !email.isString) return res.status(400).send("Missing or wrong format email")
+//     if(!name || !name.isString) return res.status(400).send("Missing or wrong format name")
+//     res.status(400).send({
+//         user:{
+//             "email":email,
+//             "username": name
+//         }
+// })      
+// })
+
+
+
+// router.post("/products/:productId", centralizedErrorHandling ,(req, res) => {
+//     const { productId } = req.query;
+//     if(!centralizedErrorHandling){
+//         if(productId.isString) res.status(400).send("Product cannot be string")
+//         const findProduct = products.findIndex((product) => products.productId === product.productId)
+//         res.status(200).send(findProduct)
+//     }else{
+//         res.send(centralizedErrorHandling)
+//     }
     
-}
-)
+// }
+// )
 
 
-const centralizedErrorHandling = ("/", (req, res, error ,next) => {
-    if(error){
-        res.status(400).send(error)
-    }
+// const centralizedErrorHandling = ("/", (req, res, error ,next) => {
+//     if(error){
+//         res.status(400).send(error)
+//     }
+//     next();
+// })
+
+
+
+
+
+
+// export default router;
+
+
+
+
+import express from "express";
+
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+
+const LoggingMiddleware = (req, res, next) => {
+
+    console.log(`${req.method} - ${req.url}`)
+    console.log(Date.now());
     next();
+}
+
+app.use(LoggingMiddleware)
+
+import jwt from "jsonwebtoken"
+
+
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    for (let index = 0; index < users.length; index++) {
+        const user = users[index]
+        if (user.email=== email){
+            if(!user.password === password)return res.status(400).send("Wrong Password")
+            res.status(200).send(`Welcome back ${user.email}`)
+        }else{
+            res.status(404).send("Email not found")
+        }       
+    }
+})
+
+
+app.get('/profile', (req, res) => {
+    const { email, password} = req.query;
+    for (let index=0; index < users.length; index++){
+        if(jwt.decode())
+    }
+
 })
 
 
@@ -214,5 +260,4 @@ const centralizedErrorHandling = ("/", (req, res, error ,next) => {
 
 
 
-export default router;
 
