9d285530f4155ca3ce8857879ff7118c4c825c31
Wairioko <charlesmungai5@gmail.com>
2024-08-09 23:41:12 +0300

    introduction to redis in nodejs

--- caching/index.mjs
+++ caching/index.mjs
@@ -1,27 +1,25 @@
 import redis from 'redis';
 import express from 'express'
-// initiate redis client
-const redisClient = redis.createClient({
-    host: 'localhost',
-    port: 7000
-})
 
 
-const app = express();
+// start redis client
 
-redisClient.on('connect', () => console.log("Connected to redis client"));
+const clientRedis = redis.createClient(
+    {
+        host:'localhost',
+        port: 7000
+    }
+)
 
 
+clientRedis.on("error", (error) => console.log
+("Could not start redis client", error));
 
-redisClient.on('error', (error) => console.log("Error starting redis", 
-    error.message))
+clientRedis.on('connect', console.log("Connected successfully to "))
 
 
-
-// middleware to check cache 
 function cache(req, res, next){
-    const {id} = req.params;
-
+    const { id } = req.params
     redisClient.get(
         id, (err, data) => {
             if(err) throw err;
@@ -36,24 +34,89 @@ function cache(req, res, next){
 }
 
 
-
-
-app.get('/api/data/:id', (req, res) => {
+app.get('/api/user/:id', (req, res) => {
     const { id } = req.params;
+    if(!id) throw err
 
-    // Simulate a database call
     const data = {
-        id: id,
-        value: `Data for ID: ${id}`
-    };
-
-    // set data to redis
-    redisClient.setEx(id, 3600, JSON.stringify(data));
-
-    redisClient.rPush()
-    res.send(data);
+        "id": "1",
+        "name": "John Doe",
+        "email": "john.doe@example.com"
+      }
+    try{
+    clientRedis.setEx(id, 3600, JSON.stringify(data));
+    clientRedis.rPush()
+    res.send(data)
+    }catch(error){
+        console.log("ERROR WHILE SENDIN DATA TO REDIS", error)
+    }
 
 })
+
+
+
+
+
+
+
+
+
+
+
+// // initiate redis client
+// const redisClient = redis.createClient({
+//     host: 'localhost',
+//     port: 7000
+// })
+
+
+// const app = express();
+
+// redisClient.on('connect', () => console.log("Connected to redis client"));
+
+
+
+// redisClient.on('error', (error) => console.log("Error starting redis", 
+//     error.message))
+
+
+
+// // middleware to check cache 
+// function cache(req, res, next){
+//     const {id} = req.params;
+
+//     redisClient.get(
+//         id, (err, data) => {
+//             if(err) throw err;
+
+//             if(data !== null) {
+//                 res.send(data);
+//             }else{
+//                 next();
+//             }
+//         }
+//     )
+// }
+
+
+
+
+// app.get('/api/data/:id', (req, res) => {
+//     const { id } = req.params;
+
+//     // Simulate a database call
+//     const data = {
+//         id: id,
+//         value: `Data for ID: ${id}`
+//     };
+
+//     // set data to redis
+//     redisClient.setEx(id, 3600, JSON.stringify(data));
+
+//     redisClient.rPush()
+//     res.send(data);
+
+// })
 
 
 app.listen(3000, () => {
